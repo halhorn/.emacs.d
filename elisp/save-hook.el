791ad2d06ba41065b21c9ecebdd7cb28c9aba23c
harumitsu.nobuta <halhorn@halmidi.com>
2017-06-09 01:51:58 +0000

    anytime, test

--- elisp/save-hook.el
+++ elisp/save-hook.el
@@ -4,14 +4,9 @@
   (interactive)
   (cond
    ;; python のテストファイルの場合テストする
-   ((and (eq major-mode 'python-mode) (string-match "/halucas/test/" (buffer-file-name)))
-    (compile
-     (format "cd %s && ./run_tests.sh %s" (find-root (buffer-file-name)) (buffer-file-name))))
-
-   ;; python ファイルの場合：flake8 する
    ((eq major-mode 'python-mode)
     (compile
-     (format "flake8 %s" (buffer-file-name))))
+     (format "cd %s && ./run_tests.sh %s" (find-root (buffer-file-name)) (buffer-file-name))))
    ))
 
 (provide 'save-hook)
