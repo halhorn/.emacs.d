c1816e14005e891c32d621b81cad2415aa85949e
harumitsu.nobuta <halhorn@halmidi.com>
2018-09-11 18:47:22 +0900

    use test/run

--- elisp/save-hook.el
+++ elisp/save-hook.el
@@ -6,7 +6,8 @@
    ;; python のテストファイルの場合テストする
    ((eq major-mode 'python-mode)
     (compile
-     (format "cd %s && flake8 %s && python -m test.run %s" (find-root (buffer-file-name)) (buffer-file-name) (buffer-file-name))))
+     ;; (format "cd %s && flake8 %s && python -m test.run %s" (find-root (buffer-file-name)) (buffer-file-name) (buffer-file-name))))
+     (format "cd %s && ./test/run %s" (find-root (buffer-file-name)) (buffer-file-name) )))
    ))
 
 (provide 'save-hook)
