fbabbf3f4ea5d7d12b98feb8636e398284cc8fe6
halhorn <halhorn@halmidi.com>
2019-07-08 16:46:26 +0900

    fix html indent

--- init.el
+++ init.el
@@ -141,7 +141,7 @@
 (add-to-list 'auto-mode-alist '("\\.tmpl?$"     . web-mode))
 (defun web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-html-offset   2)
+  (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-offset    2)
   (setq web-mode-script-offset 2)
   (setq web-mode-php-offset    2)
